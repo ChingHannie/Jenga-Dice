93d90ce5253bc3fa85c70376d0f9bf835f7a6706
ChingHannie <ching.hannie@dhs.sg>
2015-06-12 11:49:07 +0800

    Create jenga-dice.py

---
+++ jenga-dice.py
@@ -0,0 +1,21 @@
+__author__ = 'Hannie'
+
+
+import random
+x = 1
+cont = True
+while cont:
+    ok = input("Ready for next turn? (y/n) ")
+    print()
+    if ok == "y":
+        COLOURS = ("Red", "Yellow", "Green", "Blue")
+        print("TURN {}: {} {}".format(x, random.choice(COLOURS), random.randint(1, 4)))
+        print()
+        x += 1
+    elif ok == "n":
+        print("Jenga fell at TURN {}.".format(x-1))
+        print("Thank you for playing!")
+        cont = False
+    else:
+        print("Invalid choice entered. Please try again.")
+        print()
